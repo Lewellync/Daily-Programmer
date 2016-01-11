06400e334de6b94a034dfd41eb2a9e93adc7c58b
Connor Lewellyn <cdl31@pitt.edu>
2016-01-11 14:14:09 -0500

    Update Stock_Market.java

--- 249/Stock_Market.java
+++ 249/Stock_Market.java
@@ -1,16 +1,6 @@
 
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author administrator
- */
 public class Stock_Market {
     
     public static void main(String [] args) {
